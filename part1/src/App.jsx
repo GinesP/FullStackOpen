bde8583c508c5250881cbd2b98198ee0adcab5b0
GinesP <sroot1@gmail.com>
2025-02-21 13:34:07 +0100

    Agregar funcionalidad de retroalimentación con botones y estadísticas

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,49 +1,70 @@
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>
-}
+import { useState } from "react"
 
-const Part = (props) => {
-  return <p>{props.part.name} {props.part.exercises} exercises</p>
-}
-
-const Content = (props) => {
+const Button = ({ onClick, text }) => {
   return (
-    <div>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
-    </div>
+    <button onClick={onClick}>{text}</button>
+  )
+}
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+const Statistics = ({ good, neutral, bad }) => {
+
+  const total = good + neutral + bad
+  const average = ((good * 1) + (bad * -1)) / total
+  const positive = (good / total) * 100
+
+  if (total === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <table>
+      <tbody>
+        <StatisticsLine text='good' value={good} />
+        <StatisticsLine text='neutral' value={neutral} />
+        <StatisticsLine text='bad' value={bad} />
+        <StatisticsLine text='all' value={total} />
+        <StatisticsLine text='average' value={average} />
+        <StatisticsLine text='positive' value={positive + ' %'} />
+      </tbody>
+    </table>
   )
 }
 
-const Total = (props) => {
-  return <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises} exercises</p>
-}
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setBad(bad + 1)
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h1>give feedback</h1>
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <h2>statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
